8df0244cfd9dc0b920a9d3d1545ae118dc3641ea
OlenaDoroshenko <70623973+OlenaDoroshenko@users.noreply.github.com>
2021-09-26 13:59:49 +0300

    123

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,5 +3,6 @@ import dishCardTemp from '../template/dish-card.hbs';
 import { refs } from './refs.js';
 const { list } = refs;
 
+
 const dishCardsMarkup = dishCardTemp(menu);
 list.insertAdjacentHTML('beforeend', dishCardsMarkup);
